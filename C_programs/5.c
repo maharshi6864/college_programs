7a43ddb6843f49be83eab65f23b7489d0bce610e
Maharshi Patel <138155201+maharshi6864@users.noreply.github.com>
2023-07-30 21:02:24 +0530

    There is a change in file 5.c and 16.c, two logics has been made in that files.

--- C_programs/5.c
+++ C_programs/5.c
@@ -1,12 +1,26 @@
-// Write a program to swap of two numbers
+// Write a program to swap of two numbers with variables
 #include <stdio.h>
+
+// int main()
+// {
+//     int a, b;
+//     printf("Enter value of a: ");
+//     scanf("%d", &b);
+//     printf("Enter value of b: ");
+//     scanf("%d", &a);
+//     printf("\nValue of a >>%d \nValue of b >>%d", a, b);
+// }
 
 int main()
 {
-    int a, b;
+    int a, b, temp;
     printf("Enter value of a: ");
-    scanf("%d", &b);
-    printf("Enter value of b: ");
     scanf("%d", &a);
+    printf("Enter value of b: ");
+    scanf("%d", &b);
+    temp = a;
+    a = b;
+    b = temp;
+
     printf("\nValue of a >>%d \nValue of b >>%d", a, b);
 }
