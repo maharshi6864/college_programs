79a2622fede813059b5550e51d42495733e74c8e
Maharshi Patel <patelmaharshi6864@gmail.com>
2023-12-05 15:38:40 +0530

    Welcome To 2nd Semester.

--- C_practice_questions_from_college/3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-
-    float weight_1 = 15, item_1 = 5, weight_2 = 25, item_2 = 4;
-    float average_weigth = ((weight_1 * item_1) + (weight_2 * item_2)) / (item_1 + item_2);
-    printf("%f", average_weigth);
-}
