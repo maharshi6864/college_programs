f9ac864c73c2fcd558e969c3cc8693a08ab03844
Maharshi Patel <patelmaharshi6864@gmail.com>
2025-01-08 20:34:36 +0530

    Sem 4 Core Java ArrayList delete and update completed.

--- Sem4/Core_Java/ArraysAndCollection/ArrayList/ArrayListExample.java
+++ Sem4/Core_Java/ArraysAndCollection/ArrayList/ArrayListExample.java
@@ -1,24 +1,53 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class ArrayListExample {
+public class ArrayListExample {
 
-  public static void main(String[] args) {
-    // creating ArrayList Object.
-    ArrayList<Integer> arrayList = new ArrayList<>();
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter integer elements in the arraylist : ");
-    System.out.println("Enter -1 to stop entering elements : ");
-    int number = sc.nextInt();
-    while (number != -1) {
-      arrayList.add(number);
-      number = sc.nextInt();
-    }
+  // creating ArrayList Object.
+  static ArrayList<Integer> arrayList = new ArrayList<>();
 
+  static void display() {
     // Displaying all the added elements
     System.out.println("Diplaying all the added elements.");
     for (Integer i : arrayList) {
       System.out.println(i);
     }
+  }
+
+  static void deleteFromIndex(int index) {
+    arrayList.remove(index);
+  }
+
+  static void updateTheIndex(int index, int number) {
+    arrayList.set(index, number);
+  }
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter integer elements in the arraylist : ");
+    System.out.println("Enter -1 to stop entering elements : ");
+    int number = 0;
+    while (number != -1) {
+      arrayList.add(number);
+      number = sc.nextInt();
+    }
+
+    display();
+    // Delete the index from the ArrayList
+    System.out.println("Enter index to delete that index : ");
+    deleteFromIndex(sc.nextInt());
+
+    display();
+
+    // Delete the index from the ArrayList
+    System.out.println("Enter index you want to update : ");
+    int index = sc.nextInt();
+    System.out.println("Enter number you want to update with : ");
+    number = sc.nextInt();
+    updateTheIndex(index, number);
+
+    display();
+
   }
 }
